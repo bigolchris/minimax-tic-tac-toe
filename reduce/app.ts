86f8134d2f5b8c9cdf3f72e1b10ff11fbfa4fdbd
Christian Sanchez <52680827+bigolchris@users.noreply.github.com>
2023-01-11 17:07:56 -0800

    reducer added w switch cases using state for game actions

--- reduce/app.ts
+++ reduce/app.ts
@@ -1,26 +1,64 @@
 import { scoreStored } from "../types/types";
 import { deepCopy } from "../utils/utils";
 
-
 export enum GameAction {
-    SET_BOARDS = 'SET_BOARDS',
-    SET_PLAYERS = 'SET_PLAYER',
-    SET_SCORES = 'SET_SCORES',
-    SET_GAME_OVER = 'SET_GAME_OVER',
-    IS_COMPUTER = 'IS_COMPUTER',
+  SET_BOARDS = "SET_BOARDS",
+  SET_PLAYERS = "SET_PLAYER",
+  SET_SCORES = "SET_SCORES",
+  SET_GAME_OVER = "SET_GAME_OVER",
+  IS_COMPUTER = "IS_COMPUTER",
 }
 
 export interface GameActionType {
-    type: GameAction;
-    payload: | GameActionType['boards']
+  type: GameAction;
+  payload:
+    | GameState["boards"]
+    | GameState["isFirstPlayer"]
+    | GameState["isGameOver"]
+    | GameState["scores"]
+    | GameState["isComputer"];
 }
 
 export interface GameState {
-    boards: Array<Array<string>>;
-    isFirstPlayer: boolean;
-    scores: scoreStored;
-    isComputer:boolean ;
-    isGameOver: boolean;
+  boards: Array<Array<string>>;
+  isFirstPlayer: boolean;
+  scores: scoreStored;
+  isComputer: boolean;
+  isGameOver: boolean;
+}
 
+const Reducer = (state: GameState, action: GameActionType) => {
+  switch (action.type) {
+    case GameAction.SET_BOARDS:
+      const boards = action.payload as GameState["boards"];
+      const newBoards = deepCopy(boards);
+      return {
+        ...state,
+        boards: newBoards,
+      };
+    case GameAction.SET_PLAYERS:
+      return {
+        ...state,
+        isFirstPlayer: action.payload as GameState["isFirstPlayer"],
+      };
+    case GameAction.SET_SCORES:
+      return {
+        ...state,
+        scores: action.payload as GameState["scores"],
+      };
+    case GameAction.SET_GAME_OVER:
+      return {
+        ...state,
+        isGameOver: action.payload as GameState["isGameOver"],
+      };
+    case GameAction.IS_COMPUTER:
+      return {
+        ...state,
+        isComputer: action.payload as GameState["isComputer"],
+      };
+    default:
+      return state;
+  }
+};
 
-// const Reducer = (state: gameState, action: GameActionType) => {
+export default Reducer;
